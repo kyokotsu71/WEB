1df880e49a32560383aad2d9ac2e33f7ddc345d0
kyokotsu71 <shchegolevalexei@gmail.com>
2024-03-08 18:18:56 +0200

    [task 1] Access by link completed, added result image

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -1,11 +1,12 @@
 <?php
+// task #1
 /* Imagine a lot of code here */
+
 $very_bad_unclear_name = "15 chicken wings";
 
-
 // Write your code here:
-
-
+$order = &$very_bad_unclear_name; // link to $very_bad_unclear_name
+$order .= ', 1 double hemberger AND one XL coke plz'; // concatenation
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "\nYour order is: $very_bad_unclear_name."; // output
