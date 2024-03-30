71489b9c87acb6d428cb93fe619bca7231378dab
kyokotsu71 <shchegolevalexei@gmail.com>
2024-03-30 22:05:56 +0200

    [task 3] i-ii completed + image

---
+++ lab_3/code/save.php
@@ -0,0 +1,28 @@
+<?php
+function redirectToHome(): void
+{
+    header('Location: /');
+
+    exit();
+}
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])){
+    header('Location: /');
+
+    exit();
+}
+
+$category = $_POST['category'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+
+$filePath = "categories/{$category}/{$title}.txt";
+
+if (false === file_put_contents($filePath, $desc)){
+    throw new Exception('smth went wrong.');
+}
+
+chmod($filePath, 0777);
+redirectToHome();
+
+
