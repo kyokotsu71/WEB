b96ffbafa0fbf3bcb64ac2812d14cd8668d2169a
kyokotsu71 <shchegolevalexei@gmail.com>
2024-03-30 14:04:17 +0200

    [task 1b] completed + added result image

--- lab_3/code/task1.php
+++ lab_3/code/task1.php
@@ -8,3 +8,12 @@ echo "Found: <br />";
 foreach ($passed[0] as $p) {
     echo $p, "<br />";
 }
+
+//task 1 b
+$str = 'a1b2c3';
+$form = '/\d/';
+preg_match_all($form, $str, $passed);
+foreach ($passed[0] as $p) {
+    $str = str_replace($p, pow($p, 3), $str);
+}
+echo $str . "<br />";
